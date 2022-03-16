f29e295e7f9cc2338b2af81e6c337f73377f98ea
madhu20336 <madhu20@navgurukul.org>
2022-03-16 09:30:10 +0530

    User Interface

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Form from './component/Form';
+import Table from './component/Table';
+import './component/Table.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <Table/>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Form.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div className='container'>
+      <p className='text'>Sign In</p>
+      <h4>Email</h4>
+      <div className='container1'></div>
+      
+      <div className="textbox">
+        <p className="alignleft">Password</p>
+        <p className="alignright">Case sensitive</p>
+      </div>
+      <div className='container2'><h4 className='inner-text'><u>Show</u></h4></div>
+      <br></br>
+      <div className='textbox1'>
+        <div className='container3'></div>
+        <h4 className='text1'>Keep me signed in. <u>What's New?</u></h4>
+      </div>
+      <p className='text2'>Only check this box if on a personal device</p>
+      <br></br>
+      <div className='container4'><h3 className='inner-text1'>Sign In</h3></div>
+      
+      <h3 className='text3'><u>Forgot Your Password</u></h3>
+      
+    
+    
+
+    </div>
+  )
+}
+export default Form;
